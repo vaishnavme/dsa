b3542273cc94a7f1a15aec1e85fa45bdb052cf4e
Vaishnav <chandurkarvaishnav@gmail.com>
2025-11-05 11:18:26 +0530

    add: stack in js

---
+++ js/stack.js
@@ -0,0 +1,60 @@
+/**
+ * Stack:
+ *
+ * push(): Adds a single or multiple items to the top of the Stack.
+ * pop(): Removes and Returns the top item of the Stack.
+ * peek(): Returns the top item of the Stack.
+ * clear(): Removes all the items of the Stack.
+ * size(): Returns the length of the stack.
+ */
+
+class Stack {
+  #items = [];
+  #size = 0;
+  constructor() {
+    this.#items = [];
+    this.#size = 0;
+  }
+
+  push(item) {
+    this.#items[this.#size] = item;
+    this.#size++;
+  }
+
+  pop() {
+    const item = this.#items[this.#size - 1];
+    this.#items.length = this.#size - 1;
+    this.#size = this.#items.length;
+    return item;
+  }
+
+  peek() {
+    return this.#items[this.#size - 1];
+  }
+
+  getStack() {
+    return this.#items;
+  }
+
+  getSize() {
+    return this.#size;
+  }
+
+  clear() {
+    this.#items = [];
+    this.#size = 0;
+  }
+}
+
+const stack = new Stack();
+
+stack.push("A");
+stack.push("B");
+stack.push("C");
+
+console.log("print: ", stack.getStack());
+console.log("size: ", stack.getSize());
+console.log("peek: ", stack.peek());
+console.log("pop: ", stack.pop());
+console.log("print: ", stack.getStack());
+console.log("size: ", stack.getSize());
